b1c7deaf66683b1f40e8f680a85d7d5d20d92044
Carlos Gonzalez Diaz <cgonz011@gold.ac.uk>
2020-07-08 21:02:39 +0100

    Testing simplified jquery load

--- assets/js/load_includes.js
+++ assets/js/load_includes.js
@@ -6,12 +6,8 @@ function LoadNav() {
     $("#load-nav").load("_includes/nav_subpage.html");
 }
 
+// Jquery ready
+$().ready(function () {
+    $("#load-team").load("_includes/team.html");
+});
 
-(function ($) {
-    $(document).ready(
-        function () {
-            $("#load-team").load("_includes/team.html");
-        }
-    )
-
-})(jQuery);
